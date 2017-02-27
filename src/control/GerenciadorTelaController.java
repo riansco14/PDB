bb590e5fe43a974ae95cbbab6b4fb2ce805c3e97
Doda <siscom.suporte@hotmail.com>
2017-02-27 14:24:38 -0300

    Apenas um teste

--- src/control/GerenciadorTelaController.java
+++ src/control/GerenciadorTelaController.java
@@ -1,12 +1,18 @@
 package control;
 
+import DAO.HibernateUtil;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import org.hibernate.Session;
 
-public class GerenciadorTelaController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GerenciadorTelaController  {
 
     @FXML
     private AnchorPane loadBaju;
